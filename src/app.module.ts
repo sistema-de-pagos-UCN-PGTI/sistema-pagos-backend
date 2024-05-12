8a3eb7cdcd0fa46246b37358443cd21a4207e6e1
Diego González <diego.gonzalez07@alumnos.ucn.cl>
2024-05-12 16:29:43 -0400

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        synchronize: false,
+        autoLoadEntities: true,
+        useUTC: true,
+    }), 
+    AuthModule, 
+    UserModule,
+    RolesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
