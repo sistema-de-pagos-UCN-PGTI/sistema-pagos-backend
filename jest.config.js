e6f3d49577cf78b95eda7c4f5465c0105d0b1a1e
Clauido <claudio.cortes02@alumnos.ucn.cl>
2024-06-21 02:51:28 -0400

    - ready transaction controller testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src'],
+};
